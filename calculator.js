9b026fe5995f96e95858e4e21b72dd144e24dcd5
abusalehmdasif <abusaleh11.asif@gmail.com>
2024-06-24 00:08:25 +0600

    edited

---
+++ calculator.js
@@ -0,0 +1,19 @@
+const calculatorScreen = document.getElementById('calculator-screen');
+const keys = document.querySelector('.calculator-keys');
+
+keys.addEventListener('click', event => {
+    if (!event.target.matches('button')) return;
+    const value = event.target.value;
+
+    if (value === 'clear') {
+        calculatorScreen.value = '';
+    } else if (value === '=') {
+        try {
+            calculatorScreen.value = eval(calculatorScreen.value);
+        } catch {
+            calculatorScreen.value = 'Error';
+        }
+    } else {
+        calculatorScreen.value += value;
+    }
+});
