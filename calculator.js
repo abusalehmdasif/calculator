84f9426192a032fe8e83e8e3206b2d1f10fdbc0b
AsifZaman777 <eramasf16@gmail.com>
2024-06-24 00:27:24 +0600

    bug solved

--- calculator.js
+++ calculator.js
@@ -1,19 +1,21 @@
-const calculatorScreen = document.getElementById('calculator-screen');
-const keys = document.querySelector('.calculator-keys');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const calculatorScreen = document.getElementById('calculator-screen');
+    const keys = document.querySelector('.calculator-keys');
 
-keys.addEventListener('click', event => {
-    if (!event.target.matches('button')) return;
-    const value = event.target.value;
+    keys.addEventListener('click', event => {
+        if (!event.target.matches('button')) return;
+        const value = event.target.value;
 
-    if (value === 'clear') {
-        calculatorScreen.value = '';
-    } else if (value === '=') {
-        try {
-            calculatorScreen.value = eval(calculatorScreen.value);
-        } catch {
-            calculatorScreen.value = 'Error';
+        if (value === 'clear') {
+            calculatorScreen.value = '';
+        } else if (value === '=') {
+            try {
+                calculatorScreen.value = eval(calculatorScreen.value);
+            } catch {
+                calculatorScreen.value = 'Error';
+            }
+        } else {
+            calculatorScreen.value += value;
         }
-    } else {
-        calculatorScreen.value += value;
-    }
+    });
 });
